56963132f5c78cad46a23163ef0614ea4ab38f62
ankoye <ankoye@qq.com>
2021-05-21 13:29:10 +0800

    add: JasperInitializer

--- java/org/apache/catalina/startup/ContextConfig.java
+++ java/org/apache/catalina/startup/ContextConfig.java
@@ -65,6 +65,7 @@ import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.core.StandardHost;
 import org.apache.catalina.util.ContextName;
 import org.apache.catalina.util.Introspection;
+import org.apache.jasper.servlet.JasperInitializer;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 import org.apache.tomcat.Jar;
@@ -777,6 +778,8 @@ public class ContextConfig implements LifecycleListener {
         }
 
         webConfig();
+        // 添加jsp初始化器
+        context.addServletContainerInitializer(new JasperInitializer(), null);
 
         if (!context.getIgnoreAnnotations()) {
             applicationAnnotationsConfig();
